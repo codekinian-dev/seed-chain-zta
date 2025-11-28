0577fe571e0463ad515d6fc0bd7cb1196c513008
Rangga Djatikusuma Lukman <djatikusuma.dev@gmail.com>
2025-11-29 05:17:27 +0700

    Update for k6 test

--- k6_test/querytest.js
+++ k6_test/querytest.js
@@ -85,7 +85,7 @@ function getAccessToken(userIndex) {
  * Generate random batch ID from batch-1 to batch-5000
  */
 function getRandomBatchId() {
-    const batchNumber = Math.floor(Math.random() * 5000) + 1;
+    const batchNumber = Math.floor(Math.random() * 500) + 1;
     return `BATCH-${batchNumber}`;
 }
 
